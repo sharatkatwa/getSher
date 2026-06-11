11964008b129161b460e60e8dc50dc36e9fb87b0
Armanalam7790 <alamarman7790@gmail.com>
2026-06-11 23:27:00 +0530

    create user model

---
+++ server/src/model/user.model.js
@@ -0,0 +1,30 @@
+import {model , Schema} from 'mongoose'
+
+const userSchema = new Schema(
+  {
+    name: { type: String,
+         required: true,
+          trim: true },
+    email: { type: String,
+         required: true, 
+         unique: true,
+          lowercase: true,
+           trim: true },
+    password: { type: String},
+
+    role: {
+         type: String,
+         enum: Object.values(ROLES),
+          default: ROLES.SCORER
+         },
+    isDeleted: {
+         type: Boolean,
+          default: false
+         },
+  },
+  { timestamps: true }
+)
+
+const userModel  = model('user', userSchema)
+
+export default  userModel
