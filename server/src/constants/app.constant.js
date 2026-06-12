7fa060fd2c2b45422f4660164986eee3cc081fa6
Sachin Bind <sachinbind7977@gmail.com>
2026-06-12 17:23:36 +0530

    craeted modules folder and auth routes,controller,service

--- server/src/constants/app.constant.js
+++ server/src/constants/app.constant.js
@@ -4,3 +4,23 @@ export default {
   LOGGER_LEVEL: "info",
   NODE_ENV: "development",
 };
+
+
+export const app_config = {
+  jwt:{
+    accessToken:{expiresIn:'1H'},
+    refreshToken:{expiresIn:'30D'}
+  },
+  cookie: {
+    accessToken: {
+      ttpOnly: false,
+      secure: false,
+      maxAge: 60 * 60 * 1000
+    },
+    refreshToken: {
+      ttpOnly: true,
+      secure: false,
+      maxAge: 60 * 60 * 1000
+    }
+  }
+}
