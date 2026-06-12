d1b2783398eb6997f1df11426115191d4b1ca93a
sharatkatwa <sjkatwa2000@gmail.com>
2026-06-12 15:57:56 +0530

    user.model.js  format

--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -1,31 +1,31 @@
-import {model , Schema} from 'mongoose'
-import { ROLES } from '../constants/model.constants.js
+import { model, Schema } from "mongoose";
+import { ROLES } from "../constants/model.constants.js";
 
 const userSchema = new Schema(
   {
-    name: { type: String,
-         required: true,
-          trim: true },
-    email: { type: String,
-         required: true, 
-         unique: true,
-          lowercase: true,
-           trim: true },
-    password: { type: String},
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String },
 
     role: {
-         type: String,
-         enum: Object.values(ROLES),
-          default: ROLES.SCORER
-         },
+      type: String,
+      enum: Object.values(ROLES),
+      default: ROLES.SCORER,
+    },
     isDeleted: {
-         type: Boolean,
-          default: false
-         },
+      type: Boolean,
+      default: false,
+    },
   },
-  { timestamps: true }
-)
+  { timestamps: true },
+);
 
-const userModel  = model('user', userSchema)
+const userModel = model("user", userSchema);
 
-export default  userModel
+export default userModel;
