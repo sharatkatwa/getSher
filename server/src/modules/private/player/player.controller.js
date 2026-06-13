8f3d5d9908d9241155657ed93c9f81d9120a5aaf
Armanalam7790 <alamarman7790@gmail.com>
2026-06-13 21:46:00 +0530

    change try catch to asyncHandler

--- server/src/modules/private/player/player.controller.js
+++ server/src/modules/private/player/player.controller.js
@@ -1,50 +1,42 @@
 import { StatusCodes } from "http-status-codes";
 import PlayerService from "./player.service.js";
+import { buildSuccessResponse } from "../../../shared/utils/buildResponse.js";
 
 export default class PlayerController {
   constructor() {
     this.playerService = new PlayerService();
   }
 
-  async createPlayer(req, res, next) {
-    try {
-      const player = await this.playerService.createPlayer(req.body);
+  async createPlayer(req, res) {
+    const player = await this.playerService.createPlayer(req.body);
 
-      res.status(StatusCodes.CREATED).json({
-        success: true,
-        data: player,
-      });
-    } catch (error) {
-      next(error);
-    }
+    return buildSuccessResponse(
+      res,
+      player,
+      "Player created successfully",
+      StatusCodes.CREATED
+    );
   }
 
-  async updatePlayer(req, res, next) {
-    try {
-      const player = await this.playerService.updatePlayer(
-        req.params.id,
-        req.body
-      );
+  async updatePlayer(req, res) {
+    const player = await this.playerService.updatePlayer(req.params.id, req.body);
 
-      res.status(StatusCodes.OK).json({
-        success: true,
-        data: player,
-      });
-    } catch (error) {
-      next(error);
-    }
+    return buildSuccessResponse(
+      res,
+      player,
+      "Player updated successfully",
+      StatusCodes.OK
+    );
   }
 
-  async deletePlayer(req, res, next) {
-    try {
-      const player = await this.playerService.deletePlayer(req.params.id);
+  async deletePlayer(req, res) {
+    const player = await this.playerService.deletePlayer(req.params.id);
 
-      res.status(StatusCodes.OK).json({
-        success: true,
-        data: player,
-      });
-    } catch (error) {
-      next(error);
-    }
+    return buildSuccessResponse(
+      res,
+      player,
+      "Player deleted successfully",
+      StatusCodes.OK
+    );
   }
 }
--- server/src/modules/public/player/player.controller.js
+++ server/src/modules/public/player/player.controller.js
@@ -1,34 +1,31 @@
 import { StatusCodes } from "http-status-codes";
 import PlayerService from "./player.service.js";
+import { buildSuccessResponse } from "../../../shared/utils/buildResponse.js";
 
 export default class PlayerController {
   constructor() {
     this.playerService = new PlayerService();
   }
 
-  async getPlayers(req, res, next) {
-    try {
-      const players = await this.playerService.getPlayers();
+  async getPlayers(req, res) {
+    const players = await this.playerService.getPlayers();
 
-      res.status(StatusCodes.OK).json({
-        success: true,
-        data: players,
-      });
-    } catch (error) {
-      next(error);
-    }
+    return buildSuccessResponse(
+      res,
+      players,
+      "Players fetched successfully",
+      StatusCodes.OK
+    );
   }
 
-  async getPlayerById(req, res, next) {
-    try {
-      const player = await this.playerService.getPlayerById(req.params.id);
+  async getPlayerById(req, res) {
+    const player = await this.playerService.getPlayerById(req.params.id);
 
-      res.status(StatusCodes.OK).json({
-        success: true,
-        data: player,
-      });
-    } catch (error) {
-      next(error);
-    }
+    return buildSuccessResponse(
+      res,
+      player,
+      "Player fetched successfully",
+      StatusCodes.OK
+    );
   }
 }
