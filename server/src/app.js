1b3b06a5b9a0d56ee635e3443ac5c4eb981042f9
Sumit <sumitk72004@gmail.com>
2026-06-14 19:50:47 +0530

    Update app routes

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,11 @@ import ErrorHandler from "./middlewares/errorHandler.middleware.js";
 import securityMiddleware from "./middlewares/security.middleware.js";
 import googleOAuthMiddleware from "./middlewares/googleOAuth.middleware.js";
 import notFound from "./middlewares/notFound.middleware.js";
+
 // routes import statements
 import authRouter from "./modules/public/auth/auth.router.js";
-import publicMatchRouter from "./modules/public/match/match.router.js";
-import privateMatchRouter from "./modules/private/match/match.route.js";
 
-// routes import statements for private and public series
+// routes import statements for private and public series statement
 import privateSeriesRouter from "./modules/private/series/series.router.js";
 import publicSeriesRouter from "./modules/public/series/series.router.js";
 
@@ -35,10 +34,6 @@ export default function createApp() {
   });
 
   app.use("/api/auth", authRouter);
-
-  app.use("/api/matches", publicMatchRouter);
-  app.use("/api/matches", privateMatchRouter);
-
   app.use("/api/private/series", privateSeriesRouter);
   app.use("/api/series", publicSeriesRouter);
 
