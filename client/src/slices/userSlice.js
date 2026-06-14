b8184d341a9c964202217a9307aa96276c7757e7
sharatkatwa <sjkatwa2000@gmail.com>
2026-06-14 22:11:48 +0530

    frontend integration

---
+++ client/src/slices/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    name: "",
+    id: "",
+    email: "",
+    picture: "",
+    role: ""
+  },
+  reducers: {
+    setUser: (state,action) =>{
+        state.id = action.payload.id
+        state.name = action.payload.name
+        state.email = action.payload.email
+        state.picture = action.payload.picture
+        state.role = action.payload.role
+    }
+  },
+});
+
+export const {setUser} = userSlice.actions;
+export default userSlice.reducer;
