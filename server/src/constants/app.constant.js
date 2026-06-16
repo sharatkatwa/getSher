0287f9caa6ae4e096a78cfbfc464642cec210969
sharatkatwa <sjkatwa2000@gmail.com>
2026-06-16 16:48:37 +0530

    callback fix

--- server/src/constants/app.constant.js
+++ server/src/constants/app.constant.js
@@ -6,24 +6,25 @@ export default {
   RATELIMIT_WINDOWMS: 15 * 60 * 1000,
   RATELIMIT: 100,
   GOOGLE_CALLBACK_URL: 'http://localhost:3000/api/auth/google/callback',
-  REDIRECT_URL: 'http://localhost:5173'
+  GOOGLE_CALLBACK_URL: 'https://getsher.onrender.com/api/auth/google/callback',
+  // REDIRECT_URL: 'http://localhost:5173'
 };
 
 export const app_config = {
-  jwt:{
-    accessToken:{expiresIn:'1H'},
-    refreshToken:{expiresIn:'30D'}
+  jwt: {
+    accessToken: { expiresIn: "1H" },
+    refreshToken: { expiresIn: "30D" },
   },
   cookie: {
     accessToken: {
       httpOnly: false,
       secure: false,
-      maxAge: 60 * 60 * 1000
+      maxAge: 15 * 60 * 1000,
     },
     refreshToken: {
       httpOnly: true,
       secure: false,
-      maxAge: 60 * 60 * 1000
-    }
-  }
-}
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  },
+};
